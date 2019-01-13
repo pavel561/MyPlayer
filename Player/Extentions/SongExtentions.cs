fcb21241d49405582106a4479943e74484fee61a
Pavel Gornostaev <pavel561@gmail.com>
2019-01-13 23:12:27 +0300

    ShortName

--- Player/Extentions/SongExtentions.cs
+++ Player/Extentions/SongExtentions.cs
@@ -4,10 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Player.Extentions
+
+namespace MusicPlayer.Extentions
 {
 	static class SongExtentions
 	{
-		
+		const int MAX_LENGTH = 5;
+		static public String ShortName(this String name)
+		{
+			if (name.Length > MAX_LENGTH)
+			{
+				return name.Substring(0, MAX_LENGTH).Insert(MAX_LENGTH, "...");
+			}
+			else
+			{
+				return name;
+			}
+		}
+		static public List<Song> Shuffle(this List<Song> songs)
+		{
+			Random rnd = new Random();
+			for (int i = songs.Count - 1; i >= 0; i--)
+			{
+				var song = songs[rnd.Next(songs.Count - 1)];
+				songs.Remove(song);
+				songs.Add(song);
+			}
+			return songs;
+		}
 	}
 }
+
--- Player/Song.cs
+++ Player/Song.cs
@@ -6,11 +6,27 @@ using System.Threading.Tasks;
 
 namespace MusicPlayer
 {
-    class Song
+    class Song:IComparable
     {
         public int Duration;
         public string Name;
         public Artist Artist;
         public Album Album;
-    }
+
+		public int CompareTo(object obj)
+		{
+			//if (this.Name == null)
+			//    return 0;
+
+			//var songToCompare = (obj as Song);
+
+			//var result = this.Name?.CompareTo(songToCompare);
+
+			//return result ?? 0;
+
+			//return result == null ? 0 : result.Value;
+
+			return this.Name?.CompareTo((obj as Song)?.Name) ?? 0;
+		}
+	}
 }
